3307af82610bfc68e3da4e3b7b5529c2a5bb0946
junior-x <marcosmansur@outlook.com>
2022-03-18 18:00:28 -0700

    futurePast

---
+++ lib/views/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:consumindo_api/views/widgets/recipe_card.dart';
+import 'package:consumindo_api/models/recipe.dart';
+import 'package:consumindo_api/models/recipe.api.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+    print(_recipes);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.restaurant_menu),
+              SizedBox(width: 10),
+              Text('Food Recipes'),
+            ],
+          ),
+        ),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _recipes.length,
+                itemBuilder: (context, index) {
+                  return RecipeCard(
+                    title: _recipes[index].name,
+                    cookTime: _recipes[index].totalTime,
+                    rating: _recipes[index].rating.toString(),
+                    thumbnailUrl: _recipes[index].images,
+                  );
+                },
+              ));
+  }
+}
